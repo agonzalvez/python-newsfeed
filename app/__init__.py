67d1013f48705aa10b21a105167b36b558f9a993
Alejandro Gonzalvez <agonzalvez1@gmail.com>
2021-08-10 21:36:03 -0400

    Dashboard route created, and registered on init package

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from app.routes import home
+from app.routes import home, dashboard
 from flask import Flask
 
 
@@ -16,5 +17,6 @@ def create_app(test_config=None):
 
     # register routes
     app.register_blueprint(home)
+    app.register_blueprint(dashboard)
 
     return app
